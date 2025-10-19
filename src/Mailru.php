f28c99ad72955c82bf5c501f77e0574f5a8cdab7
mnwb-fh <support@fish-hook.ru>
2025-10-19 12:28:48 +0300

    Update Mailru.php

--- src/Mailru.php
+++ src/Mailru.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Jokerov\OAuth2\Client\Provider;
+namespace mnwb\OAuth2\Client\Provider;
 
-use League\OAuth2\Client\Provider\AbstractProvider;
+ovider\AbstractProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use League\OAuth2\Client\Token\AccessToken;
@@ -15,7 +15,8 @@ class Mailru extends AbstractProvider
      */
     public function getBaseAuthorizationUrl(): string
     {
-        return 'https://oauth.mail.ru/login';
+        //return 'https://oauth.mail.ru/login';
+        return 'https://connect.mail.ru/oauth/authorize';
     }
 
     /**
@@ -23,7 +24,7 @@ class Mailru extends AbstractProvider
      */
     public function getBaseAccessTokenUrl(array $params): string
     {
-        return 'https://oauth.mail.ru/token';
+        return 'https://connect.mail.ru/oauth/token';
     }
 
     /**
@@ -31,7 +32,9 @@ class Mailru extends AbstractProvider
      */
     public function getResourceOwnerDetailsUrl(AccessToken $token): string
     {
-        return 'https://oauth.mail.ru/userinfo?access_token=' . $token->getToken();
+        $param = 'app_id=' . $this->clientId . '&method=users.getInfo&secure=1&session_key=' . $token->getToken();
+        $sign = md5(str_replace('&', '', $param) . $this->clientSecret);
+        return 'http://www.appsmail.ru/platform/api?' . $param . '&sig=' . $sign;
     }
 
     /**
@@ -48,17 +51,10 @@ class Mailru extends AbstractProvider
     protected function checkResponse(ResponseInterface $response, $data): void
     {
         if (isset($data['error_code'])) {
-            throw new IdentityProviderException(
-                $data['error_description'],
-                $data['error_code'],
-                $response
-            );
+            throw new IdentityProviderException($data['error_msg'], $data['error_code'], $response);
         } elseif (isset($data['error'])) {
-            throw new IdentityProviderException(
-                $data['error'],
-                $response->getStatusCode(),
-                $response
-            );
+            throw new IdentityProviderException($data['error'],
+                $response->getStatusCode(), $response);
         }
     }
 
